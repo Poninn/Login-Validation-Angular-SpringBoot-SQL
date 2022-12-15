9a45f346d8a2f15e51961ecc6344726b548b3603
Poninn <118496738+Poninn@users.noreply.github.com>
2022-12-15 18:42:06 +0530

    Add files via upload

---
+++ Login Validation Angular SpringBoot & SQL/SpringBoot/login/src/main/java/com/login/dbutil/DBUtil.java
@@ -0,0 +1,33 @@
+package com.login.dbutil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+
+	private static Connection connection = null;   
+	
+	public static Connection getConnection() throws SQLException {
+		
+		if(connection != null) {
+			return connection;
+		}
+		else {
+			String driver = "com.mysql.cj.jdbc.Driver";
+			String url = "jdbc:mysql://localhost:3306/loginusers?useSSL=false";
+			String user = "root";
+			String password = "ponin123";
+			
+			try {
+				Class.forName(driver);
+				connection = DriverManager.getConnection(url,user,password);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return connection;
+	}
+}
---
+++ Login Validation Angular SpringBoot & SQL/SpringBoot/login/src/main/java/com/login/service/UserService.java
@@ -0,0 +1,11 @@
+package com.login.service;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserService {
+	
+	
+	public int loginValidation(String username, String password); 
+
+}
